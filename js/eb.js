daa31f755a25f7631a7482368732c242f07c68b8
potatomash22 <mathikofoed@gmail.com>
2016-06-12 00:37:37 +0200

    Fixed new overlay at ekstrabladet.dk

--- js/eb.js
+++ js/eb.js
@@ -6,5 +6,11 @@ $(document).ready(function() {
     /*
     *	Find and remove elment from dom
     */
-    $("#fnAdblockingOverlay, #fnOverlayStuff").remove();
+    var divs = $("body > div");
+    for(var i = 0; i < divs.length; i++) {
+        var div = divs[i];
+        if(div.id && div.id.length == 24) {
+            div.remove();
+        }
+    }
 });
