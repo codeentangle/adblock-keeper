d3daf872585dba04736e8a6df090a941cddc881f
mathias <mrk@cmp-company.com>
2017-02-17 10:28:26 +0100

    New structure at ekstrabladet.dk

--- js/eb.js
+++ js/eb.js
@@ -6,11 +6,15 @@ $(document).ready(function() {
     /*
     *	Find and remove elment from dom
     */
-    var divs = $("body > div");
+    var divs = $("#ebWireframe > div");
     for(var i = 0; i < divs.length; i++) {
         var div = divs[i];
         if(div.id && div.id.length == 24) {
             div.remove();
         }
     }
+    /*
+        Remove cookie
+    */
+    document.cookie = 'is_using_adblocker=false;path=/;';
 });
