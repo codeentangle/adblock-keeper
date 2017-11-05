4645c6db997ed5fdd7efdd6d7a5430ac6703b9fd
mathias <mathikofoed@gmail.com>
2017-11-05 17:01:36 +0100

    Support https for all sites and some code clean-up

--- js/jp.js
+++ js/jp.js
@@ -1,27 +1,27 @@
 /*
 *	Script to handle jyllands-posten.dk
 */
-    
-var elementid;
-/*
-*	Find full name of dynamic id
-*/
-elementid = $("[id^='overlay']").attr('id');
 
-/*
-*	Remove none unique part of string
-*/
-elementid = elementid.replace("overlay", "");
+$(document).ready(function() {
+	/*
+	*	Find full name of dynamic id
+	*/
+	var elementid = $("[id^='overlay']").attr('id');
 
-/*
-*	Remove overlay and message
-*/
-$("#overlay" + elementid + ", .userOverlay" + elementid).remove();
+	/*
+	*	Remove none unique part of string
+	*/
+	elementid = elementid.replace("overlay", "");
 
-/*
-*	Enable link functionality again - nice try JP!
-*/
-$("a").click(function() {
-	window.location = this.href;
+	/*
+	*	Remove overlay and message
+	*/
+	$("#overlay" + elementid + ", .userOverlay" + elementid).remove();
+
+	/*
+	*	Enable link functionality again - nice try JP!
+	*/
+	$("a").click(function() {
+		window.location = this.href;
+	});
 });
-
